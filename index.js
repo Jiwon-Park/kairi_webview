6ec689849105f7cab18768afe98be7569fd32391
Seniorious <wldnjs1323@naver.com>
2022-12-11 22:24:13 +0900

    uppercase

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.get('/Imgview/:card_id', (req, res) => {
 })
 
 app.get('/live2d_resource/:card_id-body.model.json', (req, res) => {
-    res.sendFile(`/live2d_resource/Live2D/${req.params['card_id']}/body.model.json`, {root: './resources'})
+    let card_id = req.params['card_id']
+    card_id[0] = card_id[0].toUpperCase()
+    res.sendFile(`/live2d_resource/Live2D/${card_id}/body.model.json`, {root: './resources'})
 })
 
 app.get('/resources/*', (req, res) => {
